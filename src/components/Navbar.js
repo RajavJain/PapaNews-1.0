d9f13c862605fc3b00c1961cd8585f631cc6fc53
RajavJain <rajavjain2002@gmail.com>
2022-11-21 22:00:32 +0530

    Added Active in NavBar and class Based Comp.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,42 +1,42 @@
-import React, { Component } from 'react'
 
 import { Link } from "react-router-dom";
-  
+import { useLocation } from 'react-router-dom';
 
-export default class navbar extends Component {
 
-    
-  render() {
- // eslint-disable-next-line
-    var addClass = (e)=>{              
-        const activeEle = document.querySelector('.active');
-        activeEle.classList.remove('active');
-        e.target.classList.add('active');
-    }
+const NavBar = () => {
 
-    return (
-        <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">PapaNews</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
 
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li><Link className="nav-link " onClick={this.addClass} aria-current="page" to="/">Home</Link></li>
-                <li><Link className="nav-link " onClick={this.addClass} aria-current="page" to="/business">Business</Link></li>
-                <li><Link className="nav-link " onClick={this.addClass} aria-current="page" to="/entertainment">Entertainment</Link> </li>
-                <li><Link className="nav-link " onClick={this.addClass} aria-current="page" to="/general">General</Link> </li>
-                <li><Link className="nav-link " onClick={this.addClass} aria-current="page" to="/health">Health</Link> </li>
-                <li><Link className="nav-link " onClick={this.addClass} aria-current="page" to="/science">Science</Link> </li>
-                <li><Link className="nav-link " onClick={this.addClass} aria-current="page" to="/sports">Sports</Link></li>
-                <li><Link className="nav-link " onClick={this.addClass} aria-current="page" to="/technology">Technology</Link> </li>
-            </ul>
-          </div>
+  let location = useLocation();
+  // eslint-disable-next-line
+  // var addClass = (e) => {
+  //   const activeEle = document.querySelector('.active');
+  //   activeEle.classList.remove('active');
+  //   e.target.classList.add('active');
+  // }
+
+  return (
+    <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">PapaNews</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li><Link  className={`nav-link ${location.pathname==="/" ? "active" : ""}`} aria-current="page" to="/">Home</Link></li>
+            <li><Link className={`nav-link ${location.pathname==="/business" ? "active" : ""}`} aria-current="page" to="/business">Business</Link></li>
+            <li><Link  className={`nav-link ${location.pathname==="/entertainment" ? "active" : ""}`} aria-current="page" to="/entertainment">Entertainment</Link> </li>
+            <li><Link  className={`nav-link ${location.pathname==="/general" ? "active" : ""}`} aria-current="page" to="/general">General</Link> </li>
+            <li><Link  className={`nav-link ${location.pathname==="/health" ? "active" : ""}`} aria-current="page" to="/health">Health</Link> </li>
+            <li><Link  className={`nav-link ${location.pathname==="/science" ? "active" : ""}`} aria-current="page" to="/science">Science</Link> </li>
+            <li><Link  className={`nav-link ${location.pathname==="/sports" ? "active" : ""}`} aria-current="page" to="/sports">Sports</Link></li>
+            <li><Link  className={`nav-link ${location.pathname==="/technology" ? "active" : ""}`} aria-current="page" to="/technology">Technology</Link> </li>
+          </ul>
         </div>
-      </nav>
-      
-    )
-  }
+      </div>
+    </nav>
+
+  )
 }
+export default NavBar
